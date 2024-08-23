7c1f97f37d3fd73a301e8447a0171cf25647c8eb
Léon Silva Erhard <l.erhard@gmail.com>
2024-08-23 00:11:13 -0300

    feat: recriated a navbar structure

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from "react";
 import './NavBar.css';
 import Image from "next/image";
@@ -10,11 +8,21 @@ interface NavBarProps {
 
 
 export const NavBar: React.FC<NavBarProps> = ({logoSrc}) => {
-    return <div className="navbar">
-            <div className="navbar__logo">
-                <Image src={logoSrc} alt="logo" width={1200} height={466}/>
-            </div>
-        <div className="navbar__menu"></div>
-
-    </div>;
+    return <header className="primary__header">
+        <a href="#">
+            <Image src={logoSrc} alt="Amalur modas" width={1200} height={466}/>
+        </a>
+        <button className="navbar__toggle-mobile" role="button" aria-controls="primary-navigation">
+            <Image className="navbar__toggle-mobile--close-icon" src={'/images/close-toggle-icon.svg'} alt="close mobile toggle" width={20} height={20} aria-hidden={true}/>
+            <Image className="navbar__toggle-mobile--hamburguer-icon" src={'/images/hamburger-toggle-icon.svg'} alt="close mobile toggle" width={20} height={20} aria-hidden={true}/>
+            <span className="hidden">Menu</span>
+        </button>
+        <nav id="primary-navigation" className="navbar">
+            <ul aria-label="Primary" className="navbar__menu">
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Sobre nós</a></li>
+                <li><a href="#">Contatos</a></li>
+            </ul>
+        </nav>
+    </header>;
 }
